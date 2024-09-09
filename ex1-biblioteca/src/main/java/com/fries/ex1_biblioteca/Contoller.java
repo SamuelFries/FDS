5b5c1ac41df819922c1c12a9eb30d7e9c7f57a66
Samuel <samuel.gfries@gmail.com>
2024-09-09 18:31:59 -0300

    commit inicial

--- ex1-biblioteca/src/main/java/com/fries/ex1_biblioteca/Contoller.java
+++ ex1-biblioteca/src/main/java/com/fries/ex1_biblioteca/Contoller.java
@@ -1,12 +1,18 @@
 package com.fries.ex1_biblioteca;
-
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/biblioteca")
 public class Contoller {
     @GetMapping
-    public String hello() {
-        return "Hello, World!";
+    public String Bemvindo() {
+        return "Bem-vindo a biblioteca";
     }
+@GetMapping("/livros")
+public  String getLicros(){
+    return "Lista de livros";
+
+}
 }
